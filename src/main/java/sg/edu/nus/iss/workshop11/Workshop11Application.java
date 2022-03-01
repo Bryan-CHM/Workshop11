5ae2a5e3ec5ff60cfb09477524b8f9f3888708e0
Bryan-CHM <chanhmbryan@gmail.com>
2022-03-01 09:36:15 +0800

    Minor Updates

--- src/main/java/sg/edu/nus/iss/workshop11/Workshop11Application.java
+++ src/main/java/sg/edu/nus/iss/workshop11/Workshop11Application.java
@@ -4,8 +4,9 @@ import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Collections;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,26 +19,31 @@ public class Workshop11Application {
 		logger.info("Workshop 11");
 		logger.debug("Workshop 11 - 1");
 		String portNumber = null;
+
 		SpringApplication app = new SpringApplication(Workshop11Application.class);
 		//SpringApplication.run(Workshop11Application.class, args);
 		DefaultApplicationArguments appArgs = new DefaultApplicationArguments(args);
-		List optVals = appArgs.getOptionValues("port");
+
+		List<String> optVals = appArgs.getOptionValues("port");
 		logger.info("optVals > " + optVals);
-		if(optVals == null || optVals.get(0) == null){
+
+		if (optVals == null || optVals.get(0) == null)
+		{
 			//portNumber = System.getProperty("PORT",DEFAULT_PORT_NUMBER);
 			portNumber = System.getenv("PORT");
-			if (portNumber == null){
+			if (portNumber == null)
+			{
 				portNumber = DEFAULT_PORT_NUMBER;
 			}
 		}
-		else{
+		else
+		{
 			portNumber = (String)optVals.get(0);
 		}
-		if(portNumber!=null){
+		if(portNumber!=null)
+		{
 			app.setDefaultProperties(Collections.singletonMap("server.port",portNumber));
 		}
  app.run(args);
 	}
-
-
 }
